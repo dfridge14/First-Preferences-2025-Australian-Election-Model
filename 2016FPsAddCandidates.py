5d6d938a909b3f2506e6b8340e7f5e87b1bc97ca
Dania Freidgeim <daniel.freidgeim@gmail.com>
2025-02-24 17:30:22 +1100

    2016FP candidate name/party extra few lines

--- 2016FPsAddCandidates.py
+++ 2016FPsAddCandidates.py
@@ -5,7 +5,8 @@ import os, time
 os.chdir('C:\\Dania\\2024\\Australian Election')
 
 SenateCandidates_2016 = pd.read_csv("2016SenateCandidates.csv", index_col = None)
-SenateCandidates_2016 = SenateCandidates_2016.loc[SenateCandidates_2016["nom_ty"] == 'S',:]
+SenateCandidates_2016 = SenateCandidates_2016.loc[SenateCandidates_2016["nom_ty"] == 'S',["state_ab","ticket","party_ballot_nm"]]
+SenateCandidates_2016.rename(columns={"state_ab": "StateAb", "party_ballot_nm": "party_nm"})
 
 import pdb;pdb.set_trace()
 
